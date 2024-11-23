ab34556c6a4bf5e49153f08f36284eec42e08184
곽명철 <5645039@stu.kmu.ac.kr>
2024-11-24 05:00:24 +0900

    yml change commit

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 # 변수 설정
-REPOSITORY=/home/ec2-user/cicd_test  # 배포할 경로
-APP_NAME=cicd_test                   # 애플리케이션 이름
+REPOSITORY=/home/ec2-user/cicd-project # 배포할 경로
 
 # 프로젝트 디렉토리로 이동
 cd $REPOSITORY
 
 # 최신 JAR 파일 찾기
+APP_NAME=cicd_test
 JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep 'SNAPSHOT.jar' | tail -n 1)
 JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 
@@ -15,7 +15,8 @@ JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 CURRENT_PID=$(pgrep -f $APP_NAME)
 
 # 애플리케이션 중단
-if [ -z "$CURRENT_PID" ]; then
+if [ -z "$CURRENT_PID" ];
+then
   echo "> 실행 중인 애플리케이션이 없습니다."
 else
   echo "> 실행 중인 애플리케이션 종료 중: $CURRENT_PID"
