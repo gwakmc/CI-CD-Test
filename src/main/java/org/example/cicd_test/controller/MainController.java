ca116ef1414c013f78808e1a8c60311139f89cbf
곽명철 <5645039@stu.kmu.ac.kr>
2024-11-23 22:54:27 +0900

    test commit

---
+++ src/main/java/org/example/cicd_test/controller/MainController.java
@@ -0,0 +1,14 @@
+package org.example.cicd_test.controller;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+    @PostMapping("/hello")
+    public ResponseEntity<?> hello(){
+        return ResponseEntity.ok("Hello World!");
+    }
+}
