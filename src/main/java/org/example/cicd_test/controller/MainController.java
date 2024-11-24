7297649192f19bd4542d0760fc9fba5a257c4acb
곽명철 <5645039@stu.kmu.ac.kr>
2024-11-24 17:42:10 +0900

    CorsChange

--- src/main/java/org/example/cicd_test/controller/MainController.java
+++ src/main/java/org/example/cicd_test/controller/MainController.java
@@ -2,11 +2,13 @@ package org.example.cicd_test.controller;
 
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MainController {
+    @CrossOrigin("origins = \"http://localhost:8081\"")
     @GetMapping("/hello")
     public ResponseEntity<?> hello(){
         return ResponseEntity.ok()
