fa0547fdb93856754f4a8e2f422f91e42d5b3256
곽명철 <5645039@stu.kmu.ac.kr>
2024-11-24 17:36:59 +0900

    GetChange

--- src/main/java/org/example/cicd_test/controller/MainController.java
+++ src/main/java/org/example/cicd_test/controller/MainController.java
@@ -2,12 +2,12 @@ package org.example.cicd_test.controller;
 
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MainController {
-    @PostMapping("/hello")
+    @GetMapping("/hello")
     public ResponseEntity<?> hello(){
         return ResponseEntity.ok()
                 .header("hello", "cicd")
